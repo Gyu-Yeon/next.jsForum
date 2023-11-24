41e8821f01452e3e8dda96af54d7599eca90b7ab
Gyu-Yeon <idsopy@nate.com>
2023-11-25 00:33:44 +0900

    3

--- app/page.js
+++ app/page.js
@@ -1,4 +1,10 @@
-export default function Home() {
+import { connectDB } from "@/util/database.js";
+
+export default async function Home() {
+  const client = await connectDB;
+  const db = client.db("forum");
+  let result = await db.collection("post").find().toArray();
+  console.log(result);
   return (
     <div>
       <p>안녕</p>
