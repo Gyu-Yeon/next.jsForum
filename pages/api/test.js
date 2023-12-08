ba76d3b3283400436e04b0cf015a9a26c9385a3d
Gyu-Yeon <idsopy@nate.com>
2023-12-08 17:09:05 +0900

    8

--- pages/api/test.js
+++ pages/api/test.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   if (req.method === "POST") {
     return res.status(200).json("done");
   } else if (req.method === "GET") {
-    console.log(res);
+    // console.log(res);
     return res.status(200).json("d");
   }
 }
