40156df9ac5a318bd0440f5ba5d466a0421ada3c
Gyu-Yeon <idsopy@nate.com>
2023-12-09 15:19:42 +0900

    9

--- app/write/page.js
+++ app/write/page.js
@@ -3,6 +3,8 @@ export default function Write() {
     <div>
       <h4>글작성</h4>
       <form action="api/test" method="POST">
+        <input type="text" name="emial" id="EMAIL" />
+        <input type="password" name="password" id="PW" />
         <button type="submit">버튼</button>
       </form>
     </div>
--- pages/api/test.js
+++ pages/api/test.js
@@ -1,5 +1,6 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
+    console.log(req);
     return res.status(200).json("done");
   } else if (req.method === "GET") {
     // console.log(res);
