f24f4c4ab395c97de90d418b25dcb524a3c7a243
Gyu-Yeon <idsopy@nate.com>
2023-12-16 01:13:40 +0900

    14

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -1,14 +1,14 @@
 import { connectDB } from "@/util/database";
 
-export default async function handler(요청, 응답) {
-  if (요청.method == "POST") {
-    if (요청.body.title == "") {
+export default async function handler(req, res) {
+  if (req.method == "POST") {
+    if (req.body.title == "") {
       return 응답.status(500).json("제목써라");
     }
     try {
       let db = (await connectDB).db("forum");
-      let result = db.collection("post").insertOne(요청.body);
-      응답.redirect(302, "/list");
+      let result = db.collection("post").insertOne(req.body);
+      res.redirect(302, "/list");
     } catch (error) {
       console.log(error);
     }
