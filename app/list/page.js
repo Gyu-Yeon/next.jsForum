1e25b742651abac2a2658761cc180fb032f3f61c
Gyu-Yeon <idsopy@nate.com>
2023-12-29 12:34:55 +0900

    19

--- app/list/page.js
+++ app/list/page.js
@@ -5,7 +5,6 @@ import DetailLink from "./DetailLink";
 export default async function List() {
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
-  //   console.log(result);
 
   return (
     <div className="list-bg">
