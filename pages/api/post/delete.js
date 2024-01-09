d8f9c3dfe699ddfc319bdeea66d8b09aec81a76c
Gyu-Yeon <idsopy@nate.com>
2024-01-09 22:20:37 +0900

    29

--- pages/api/post/delete.js
+++ pages/api/post/delete.js
@@ -5,7 +5,9 @@ export default async function handler(req, res) {
   if (req.method == "DELETE") {
     console.log(req.body);
     const db = (await connectDB).db("forum");
-    // const result = await db.collection("post").deleteOne({ _id: 123 });
-    res.status(200).json("complete");
+    const result = await db
+      .collection("post")
+      .deleteOne({ _id: new ObjectId(req.body) });
+    res.status(200).json("삭제완료");
   }
 }
