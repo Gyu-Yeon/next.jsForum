f953f1d4394fdc8e375a7f8346d69acc18ffd6d5
Gyu-Yeon <idsopy@nate.com>
2024-01-23 21:15:26 +0900

    36

---
+++ pages/api/auth/[...netauth].js
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: "6dde8880d65559e2749f",
+      clientSecret: "eb0b9d857d5839c597c127e5b709a8af8e532463",
+    }),
+  ],
+  secret: "jwt생성시쓰는암호",
+};
+export default NextAuth(authOptions);
