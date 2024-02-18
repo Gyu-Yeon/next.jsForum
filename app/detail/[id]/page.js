c3cbdfe59416c06430ca65c58c67b064e906ae96
Gyu-Yeon <idsopy@nate.com>
2024-02-18 23:07:15 +0900

    detail page

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -1,15 +1,19 @@
 import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
 
 export default async function Detail(props) {
+  console.log(props.params.id);
   const db = (await connectDB).db("forum");
-  let result = await db.collection("post").findOne({ title: "안녕" });
-  console.log(props);
+  let result = await db
+    .collection("post")
+    .findOne({ _id: new ObjectId(props.params.id) });
+  console.log(result);
 
   return (
     <div>
       <h4>상세페이지임</h4>
-      <h4>글제목</h4>
-      <p>글내용</p>
+      <h4>{result.title}</h4>
+      <p>{result.content}</p>
     </div>
   );
 }
