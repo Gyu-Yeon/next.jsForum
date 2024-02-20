28c25cf72ed950db5c13deaf1e02bc30c2f34ce2
Gyu-Yeon <idsopy@nate.com>
2024-02-20 23:46:29 +0900

    Comment2

--- pages/api/post/comment.js
+++ pages/api/post/comment.js
@@ -5,9 +5,7 @@ export default async function handler(req, res) {
   if (req.method == "POST") {
     console.log(req.body);
     // const db = (await connectDB).db("forum");
-    // const result = await db
-    //   .collection("post")
-    //   .deleteOne({ _id: new ObjectId(req.body) });
+    // const result = await db.collection("comment").insertOne(req.body);
     // res.status(200).json("삭제완료");
   }
 }
--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -8,6 +8,7 @@ export default async function handler(req, res) {
     try {
       let db = (await connectDB).db("forum");
       let result = db.collection("post").insertOne(req.body);
+      console.log(req.body);
       res.redirect(302, "/list");
     } catch (error) {
       console.log(error);
